612e803923419437e0b04e36131cd10df5c7499c
frinesar <frinesar@gmail.com>
2022-08-19 12:18:23 +0300

    Preparing for deploy

--- todolist/settings/base.py
+++ todolist/settings/base.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+import sqlite3
 import dj_database_url
-from decouple import config
+from dotenv import load_dotenv, find_dotenv
+
 
 
 
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todoapp',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -84,11 +86,11 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+load_dotenv(find_dotenv)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3',
+    conn_max_age=600, ssl_require=False)
 }
 
 
@@ -146,4 +148,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
---
+++ todolist/settings/prod.py
@@ -0,0 +1,10 @@
+import django_heroku
+from decouple import config
+from .base import *
+
+
+SECRET_KEY = config('SECRET_KEY')
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
